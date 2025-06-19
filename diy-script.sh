82c99ec098fee9c8c72539223f39d36f08904e13
z6211563 <z6211563@gmail.com>
2025-06-19 22:39:45 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -39,6 +39,7 @@ git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-a
 git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
 git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
+git clone --depth=1 https://github.com/dysf888/OpenWRT_NeZha_V1 package/OpenWRT_NeZha_V1
 
 # 科学上网插件
 #git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
