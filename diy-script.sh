43322ef4e6ebadece93d38d646dc760b5c7700a4
z6211563 <z6211563@gmail.com>
2025-06-20 20:57:59 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -45,7 +45,7 @@ git clone --depth=1 https://github.com/dysf888/OpenWRT_NeZha_V1 package/OpenWRT_
 # 科学上网插件
 #git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 #git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
